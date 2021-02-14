9c5668f77a22120094f02a363ad12e3eb7808aff
atteyo-th <78345265+atteyo-th@users.noreply.github.com>
2021-02-14 22:23:29 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,15 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    # DESC は降順という意味
+    @posts = Post.all.order(id: "DESC") 
   end
 
-  def new
-  end
+  # def new　　削除
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
+  
 end
